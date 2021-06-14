dd6ae8a89907e25f02be170364b3308f2743503b
Luis Castañeira <52086707+Solidlucho83@users.noreply.github.com>
2021-06-14 10:55:21 -0300

    v2

--- moneda.sol
+++ moneda.sol
@@ -6,14 +6,14 @@ contract Cripto {
     
     string nombre;
     string simbolo;
-    
-    
     mapping (address => uint) balance;
     uint fondosdisponibles;
+    uint fondosCirculantes;
     address owner;
     
     constructor(uint suministros, string memory nombreNuevo, string memory simboloNuevo) {
         fondosdisponibles = suministros;
+        fondosCirculantes = suministros;
         nombre = nombreNuevo;
         simbolo = simboloNuevo;
         owner = msg.sender;
@@ -23,18 +23,16 @@ contract Cripto {
         return balance[msg.sender];
     }
     
-    function CambiarOwner(address nuevoOwner) public {
+    function CambiarOwner(address nuevoOwner) public SoloOwner {
         require(owner == msg.sender, "Solo el owner puede asignar un nuevo owner.");
         owner = nuevoOwner;
         
     }
     
-    function FondearSaldo (address direccion, uint monto) public{
+    function FondearSaldo (address direccion, uint monto) public SoloOwner {
         //establesco que el owner no se pueda cargar valor
         require(direccion != owner, "El owner no se puede cargar saldo a si mismo.");
-         
-        //valido que sea unicamente el owner (dueño) el que pueda ejecutar esta funcion.
-        require(owner == msg.sender, "Solo el owner puede realizar esta accion.");
+        
         
         //require para no cargar mas saldo del que hay, devuelve  false sino
         require(fondosdisponibles >= monto, "No hay fondos suficientes." );
@@ -51,3 +49,32 @@ contract Cripto {
         balance[destino] += monto; 
         
     }
+    
+    modifier SoloOwner (){
+         require(owner != msg.sender, "Solo el owner puede ejecutar la function.");
+         _;
+    }
+    
+    function Cobrar() public payable{
+        //va vacia.
+    }
+    
+    function RetirarTokens() public SoloOwner {
+        //a partir de la version 4 , solo puede retirar address con asignacion payable.
+        address payable ownerPagable = payable(owner);
+        ownerPagable.transfer(10 wei); 
+    }
+    
+    function QuemarTokens(uint monto) public SoloOwner {
+        require( monto <= fondosdisponibles, "No hay suficientes fondos disponibles para quemar.");
+        balance[address(0)] += fondosdisponibles;
+        fondosdisponibles = 0;
+        fondosCirculantes -= monto;
+    }
+    
+    function VerCirculante() public view returns (uint){
+        return fondosCirculantes;
+    }
+    
+    
+}    
